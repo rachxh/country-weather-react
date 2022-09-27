73cccdaa4e612b5799b5e3065087d70cf83237d6
Xiaohong Chen <s2200116@BC-C02GR0BGQ6LR-MacBook-Air.local>
2022-09-27 16:37:38 +0300

    adjusted responsive view

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import style from "./Search.module.css"
-import { FaSearch } from "react-icons/fa";
+
 
 const Search = (props) => {
 
@@ -12,10 +12,6 @@ const Search = (props) => {
     return (
         <div >
             <form className={style.search_form}>
-
-                <span >
-                    <FaSearch />
-                </span>
                 <input type="search" className={style.search_bar}
                     placeholder="Search for Countries"
                     onChange={updateSearch}
