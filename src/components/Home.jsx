389e4856b6708443b684e066b08c8c1ce31799b4
Xiaohong Chen <s2200116@BC-C02GR0BGQ6LR-MacBook-Air.local>
2022-09-27 15:14:57 +0300

    responsive view

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,8 +6,8 @@ const Home = () => {
             <div>
                 <h1 className='home_title'> Learn about countries and bookmark countries you have been to in Country-App 🌏</h1>
             </div>
-            <div className='home_image'>
-                <img src="https://cdn.pixabay.com/photo/2017/01/12/13/23/map-1974699__480.png" alt="world map" />
+            <div className='home_image_container'>
+                <img className='home_img' src="https://cdn.pixabay.com/photo/2017/01/12/13/23/map-1974699__480.png" alt="world map" />
             </div>
         </>
     );
